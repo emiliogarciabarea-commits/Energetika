f1a6b0f03cbc8db6224cc5e2255f3bc25b623cb8
emiliogarciabarea-commits <emiliogarciabarea@gmail.com>
2026-03-16 20:22:40 +0100

    Update ENergetika.py

--- ENergetika.py
+++ ENergetika.py
@@ -40,7 +40,6 @@ def generar_pdf(df_detalle, df_ranking, df_consumos, nombre_cliente, direccion_c
         nombre_ganadora = ganadora_row.iloc[0]
         ahorro_total_periodo = ganadora_row.iloc[1]
         
-        # Sincronización de fechas
         lista_fechas = df_consumos['Fecha'].unique()
 
         # --- SECCIÓN: DATOS DEL CLIENTE ---
@@ -137,7 +136,7 @@ def generar_pdf(df_detalle, df_ranking, df_consumos, nombre_cliente, direccion_c
         pdf.set_font('Arial', '', 12)
         pdf.multi_cell(0, 10, f"Tras analizar su historial de consumo, la opción más eficiente para su suministro es la tarifa:", align='C')
         
-        # BLOQUE CENTRADO DE LA TARIFA
+        # --- BLOQUE CENTRADO ---
         pdf.set_font('Arial', 'B', 16)
         pdf.set_text_color(20, 50, 100)
         pdf.cell(0, 15, f"{str(nombre_ganadora).upper()}", ln=True, align='C')
